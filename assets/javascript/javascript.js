b2db94552d0e22401beac142bfe23645206e000f
EmrenIng <Ingvoldstad.Maya@gmail.com>
2017-12-13 18:20:47 -0600

    l

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,3 @@
-//document rdy fucntion.
 // Variables
 var characters = {
 	"pOne": {
@@ -10,21 +9,21 @@ var characters = {
 	},
 	"pTwo": {
 		name: "Siamese",
-		hp: 120,
+		hp: 100,
 		atk: 8,
 		imageUrl: "",
 		cAtk: 15,
 	},
 	"pThree": {
 		name: "Forest Cat",
-		hp: 120,
+		hp: 150,
 		atk: 8,
 		imageUrl: "",
 		cAtk: 15,
 	},
 	"pFour": {
 		name: "Bengal",
-		hp: 120,
+		hp: 180,
 		atk: 8,
 		imageUrl: "",
 		cAtk: 15,
@@ -32,9 +31,7 @@ var characters = {
 }
 console.log(characters);
 
-
-
-
+// Making the Characters show up on screen. 
  $(".cha-select").append('<div id="pOne" class="cha-sel"> ' +  characters.pOne.name + ' </div>');
  $(".cha-select").append('<div id="pTwo" class="cha-sel"> ' +  characters.pTwo.name + ' </div>');
  $(".cha-select").append('<div id="pThree" class="cha-sel"> ' +  characters.pThree.name + ' </div>');
@@ -42,17 +39,31 @@ console.log(characters);
 
 
 
+
+
 // Append to to move Ch between sections.
+var playerOne = $( "#pOne" ).click(function() {
+	$("#pOne").detach().appendTo('.en-select');
 
 
-//$(".cha-select").addClass( "pOne" );
-//$(".pOne").append("Hello <b>world</b>! " + characters.pOne.name + characters.pOne.hp);
+// if statement for if chosen player is empty move there if not ignore. 
+// else move it to Enemies avaliable.
+if ('cha-select' === 'chaselect:empty')
+{
+	playerOne;
+} else {
 
-//$(".cha-select").addClass( "pTwo" );
-//$(".pTwo").html("Hello <b>world</b>! " + characters.pTwo.name + characters.pTwo.hp);
+	// do not allow it to be filled. 
+}
+
+// If  fight selection is empty move there
+// Else noone.
+
+// Attack button that knows which two to set against each other. 
+
+// Hide reset till on win loss condition. Clear all areas and return players to start. 
+    
+})
 
 
-// Your Character Section
-// Enemies to attack
-// Defenders
-// for each. 
+
